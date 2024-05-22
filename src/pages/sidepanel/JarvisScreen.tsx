8de8b8c55cf5a95ac4a3d7540a6777fb2f650073
mohammadkaif0211 <mohammadkaif@cofactory.ai>
2024-05-22 13:08:43 +0530

    Revert "proper API Handling"
    This reverts commit 19eba176379cced63b4d8caeff028dd1fc0eaaa4.

--- src/pages/sidepanel/JarvisScreen.tsx
+++ src/pages/sidepanel/JarvisScreen.tsx
@@ -13,13 +13,13 @@ const JarvisScreen = () => {
   const meetingIdRef = useRef<number | null>(null);
   const [repText, setRepText] = useState<string>("");
   const timerRef = useRef<NodeJS.Timeout | null>(null);
+  const [endMeetingResponseType, setEndMeetingResponseType] =
+    useState<string>();
   const [asanaTask, setAsanaTask] = useState<string[]>();
   const [slackSummary, setSummarySlack] = useState<string>();
-  const [isInitailAPICallComplete, setIsInitialAPICallComplete] =
-    useState<boolean>(false);
-  const [endAPICallAction, setEndAPICallAction] = useState<string>();
+  const [apiCallingStart, setAPICallingStart] = useState<boolean>(false);
+  const [selectedTasks, setSelectedTasks] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
-  const [createAsanaAPICall, setCreateAsanaAPICall] = useState<boolean>(false);
 
   useEffect(() => {
     chrome.runtime.onMessage.addListener((request) => {
@@ -45,31 +45,29 @@ const JarvisScreen = () => {
   useEffect(() => {
     // Start or reset the timer when repText changes
     if (repText.length) {
-      if (timerRef.current) clearTimeout(timerRef.current);
+      if (timerRef.current) clearTimeout(timerRef.current); // Reset the timer if already set
       timerRef.current = setTimeout(() => {
-        if (!endAPICallAction) {
-          setIsInitialAPICallComplete(true);
-        } else if (
-          isInitailAPICallComplete &&
-          endAPICallAction === "create_asana_tasks"
-        ) {
-          setCreateAsanaAPICall(true);
-        }
+        // Call the API after 4 seconds if repText hasn't changed
+        setAPICallingStart(true);
+        console.log("API call after 4 seconds");
       }, 4000);
     }
   }, [repText]);
 
   useEffect(() => {
-    if (isInitailAPICallComplete && !endAPICallAction) {
+    if (apiCallingStart && !endMeetingResponseType) {
       handleRecordingStart().then(() => {
         sendTranscriptToBackend(meetingIdRef.current);
       });
-    } else if (isInitailAPICallComplete && createAsanaAPICall) {
+    } else if (
+      apiCallingStart &&
+      endMeetingResponseType === "create_asana_tasks"
+    ) {
       handleCreateAsanaTask().then(() => {
         console.log("asana task created successfully");
       });
     }
-  }, [isInitailAPICallComplete, createAsanaAPICall]);
+  }, [apiCallingStart, endMeetingResponseType]);
 
   async function handleRecordingStart() {
     const { id } = await addAndGetMeetingInfo();
@@ -101,10 +99,10 @@ const JarvisScreen = () => {
       const { data, type } = response.data;
 
       if (type === "create_asana_tasks" && data) {
-        setEndAPICallAction(type);
+        setEndMeetingResponseType(type);
         setAsanaTask(data);
       } else if (type === "send_summary_to_slack") {
-        setEndAPICallAction(type);
+        setEndMeetingResponseType(type);
         setSummarySlack(data);
       }
     } catch (error) {
@@ -114,6 +112,14 @@ const JarvisScreen = () => {
       setRepText("");
     }
   }
+
+  const handleCheckboxChange = (task: string) => {
+    setSelectedTasks((prevSelectedTasks) =>
+      prevSelectedTasks.includes(task)
+        ? prevSelectedTasks.filter((t) => t !== task)
+        : [...prevSelectedTasks, task]
+    );
+  };
 
   const handleCreateAsanaTask = async () => {
     try {
@@ -147,7 +153,7 @@ const JarvisScreen = () => {
     } catch (error) {
       toast.error("Failed");
       console.error("Error Failed Create Asana API", error);
-    }
+    } 
   };
 
   const handleSendSlackSummary = async () => {
@@ -161,6 +167,7 @@ const JarvisScreen = () => {
       }
       const postData = {
         meetingId: meetingIdRef.current,
+        transcription: selectedTasks,
       };
 
       // Make the POST request using Axios
@@ -174,7 +181,6 @@ const JarvisScreen = () => {
           },
         }
       );
-
       if (response.data.error) {
         throw new Error(response.data.error);
       }
@@ -200,24 +206,40 @@ const JarvisScreen = () => {
         </h2>
         {!!repText.length && <span className="text-lg">{repText}</span>}
         <div className="flex flex-col gap-1 ml-2 mt-2">
-          {endAPICallAction === "create_asana_tasks" &&
+          {endMeetingResponseType === "create_asana_tasks" &&
             asanaTask &&
             asanaTask.map((task, index) => (
               <div className="flex gap-2 p-1" key={index}>
+                {/* <input
+                  type="checkbox"
+                  value={task}
+                  checked={selectedTasks.includes(task)}
+                  onChange={() => handleCheckboxChange(task)}
+                  className="text-black  rounded"
+                /> */}
                 <span>{index + 1}</span>
                 <p className="text-sm text-start font-medium text-gray-900">
                   {task}
                 </p>
               </div>
             ))}
+          {/* {endMeetingResponseType === "create_asana_tasks" && (
+            <button
+              onClick={handleCreateAsanaTask}
+              className="m-2 px-2 py-2 flex justify-center bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+            >
+              {loading ? <Loader /> : "Create Asana Task"}
+            </button>
+          )} */}
         </div>
         <div className="flex flex-col gap-1 ml-2 mt-2">
-          {endAPICallAction === "send_summary_to_slack" && slackSummary && (
-            <p className="text-sm text-start font-medium text-gray-900">
-              {slackSummary}
-            </p>
-          )}
-          {endAPICallAction === "send_summary_to_slack" && (
+          {endMeetingResponseType === "send_summary_to_slack" &&
+            slackSummary && (
+              <p className="text-sm text-start font-medium text-gray-900">
+                {slackSummary}
+              </p>
+            )}
+          {endMeetingResponseType === "send_summary_to_slack" && (
             <button
               onClick={handleSendSlackSummary}
               className="m-2 px-2 py-2 flex justify-center bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
